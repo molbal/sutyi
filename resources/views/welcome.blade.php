8e56b1b7d070dda5e1d67342ebcdb9a3f2979b45
Balint Molnar <molbal@outlook.com>
2021-03-31 13:21:17 +0200

    Design-o

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,55 +6,71 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="{{asset('image-picker.css')}}" rel="stylesheet">
 
-    <title>Sutyi</title>
+    <title>Brainlet generátor</title>
 </head>
 <body>
 
+<form action="{{route('generate')}}" method="post" target="_blank">
 <div class="container">
     <div class="row mt-5">
-        <div class="col-sm-12">
-            <h3>Sutyi generátor</h3>
+        <div class="col-sm-12 mb-3">
+            <h3>Brainlet generátor</h3>
         </div>
     </div>
     <div class="row">
-        <form action="{{route('generate')}}" method="post" target="_blank">
-            @csrf
-            <div class="col-sm-12">
-                <div class="form-group">
-                    <label for="">Név</label>
-                    <input name="name" type="text" class="form-control">
+        <div class="card card-body shadow-sm">
+                @csrf
+                <div class="col-sm-12">
+                    <div class="form-group mt-3">
+                        <label for="">Név</label>
+                        <input name="name" type="text" class="form-control">
+                    </div>
                 </div>
-            </div>
-            <div class="col-sm-12">
-                <div class="form-group">
-                    <label for="">Komment</label>
-                    <textarea name="comment" class="form-control"></textarea>
+                <div class="col-sm-12">
+                    <div class="form-group mt-3">
+                        <label for="">Komment</label>
+                        <textarea name="comment" class="form-control"></textarea>
+                    </div>
                 </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label for="">Fej típusa</label>
-                    <select name="brainlet" id="" class="form-control">
-                        <option value="">Véletlen</option>
-                        @foreach($brainlets as $brainlet)
-                            <option value="{{$brainlet}}">{{\Illuminate\Support\Str::of($brainlet)->replace('-',' ')->replace('faces/brainlet', '')->replace('.jpg', '')->trim()->ucfirst()}}</option>
-                        @endforeach
-                    </select>
+                <div class="col-sm-12">
+                    <div class="form-group mt-3">
+                        <label for="">Brainlet</label>
+                        <select name="brainlet" id="brainlet" class="form-control ">
+                            <option data-img-src="{{asset('faces/random.png')}}" value="">Véletlen</option>
+                            @foreach($brainlets as $brainlet)
+                                <option data-img-src="{{asset($brainlet)}}"
+                                        value="{{$brainlet}}">{{\Illuminate\Support\Str::of($brainlet)->replace('-',' ')->replace('faces/brainlet', '')->replace('.jpg', '')->trim()->ucfirst()}}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
-            </div>
-            <div class="col-sm-12">
-                <input type="submit" class="mt-3 btn btn-primary" value="Mehet">
-            </div>
-        </form>
+        </div>
+        <div class="card-footer">
+            <input type="submit" class="btn btn-primary" value="Mehet">
+        </div>
     </div>
 </div>
+</form>
 
 <!-- Optional JavaScript; choose one of the two! -->
 
 <!-- Option 1: Bootstrap Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+        crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="{{asset('image-picker.min.js')}}"></script>
+{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.18/js/bootstrap-select.min.js" integrity="sha512-yDlE7vpGDP7o2eftkCiPZ+yuUyEcaBwoJoIhdXv71KZWugFqEphIS3PU60lEkFaz8RxaVsMpSvQxMBaKVwA5xg==" crossorigin="anonymous"></script>--}}
+{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.18/js/bootstrap-select.min.js" integrity="sha512-yDlE7vpGDP7o2eftkCiPZ+yuUyEcaBwoJoIhdXv71KZWugFqEphIS3PU60lEkFaz8RxaVsMpSvQxMBaKVwA5xg==" crossorigin="anonymous"></script>--}}
+<script>
+    $(function () {
+        $("select#brainlet").imagepicker()
+    });
+</script>
+<script>
+</script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,17 @@
 
     Route::post('/generate', function (\Illuminate\Http\Request $request){
 
-        $img = Image::make(Storage::disk('local')->get('base.png'));
-
-
+        $img = Image::make(Storage::disk('local')->get('white.png'));
         if (!$request->filled('brainlet')) {
             $brainlets = collect(Storage::disk('local')->allFiles('faces'));
-
-
             $img->insert(Storage::get($brainlets->random()), 'bottom-left');
         }
-        else {
+        else if(Storage::disk('local')->exists($request->get('brainlet'))) {
 
             $img->insert(Storage::get($request->get('brainlet')), 'bottom-left');
         }
+        $img->insert(Storage::disk('local')->get('base.png') );
+
 
 
         $im = $img->getCore();
@@ -39,21 +37,19 @@
         $box = new Box($im);
         $box->setFontFace(storage_path("app").'/font.ttf'); // http://www.dafont.com/minecraftia.font
         $box->setFontColor(new Color(0, 0, 0));
-//    $box->setTextShadow(new Color(0, 0, 0, 50), 2, 2);
-        $box->setFontSize(13.5);
         $box->setLineHeight(1.2);
-        $box->setBox(130,50, 350, 320);
+        $box->setBox(130+128,50, 350, 320);
         $box->setTextAlign('center', 'center');
-        $box->draw($request->get('comment'));
 
+        $box->drawFitFontSize($request->get('comment'), 1, 64, 10);
 
         $box->setFontSize(16);
 
         $img->setCore($im);
-        $box->setBox(200,433, 290, 55);
+        $box->setBox(200+128,433, 290, 55);
         $box->setTextAlign('left', 'center');
-        $box->setFontSize(18);
-        $box->draw("- ".$request->get('name')."\n".now()->toDateTimeString());
+        $box->setFontSize(32);
+        $box->draw("- ".$request->get('name'));
 
 
         return $img->response('png');
